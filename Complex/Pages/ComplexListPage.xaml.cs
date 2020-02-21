54a97cdfebf6b432499598c5425cce1f00252b70
G1Ryslan1G <Tukalovrd2@mail.ru>
2020-02-21 21:48:01 +0300

    fix using

--- Complex/Pages/ComplexListPage.xaml.cs
+++ Complex/Pages/ComplexListPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Complex.Pages
 {
@@ -62,6 +51,14 @@ namespace Complex.Pages
                     case MessageBoxResult.No:
                         break;
                 }
+            }
+        }
+
+        private void EditOPEN_Click(object sender, RoutedEventArgs e)
+        {
+            if (ComplexLV.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите комплекс", "Ошибка");
             }
         }
     }
